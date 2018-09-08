e3177b865929c0cf4283a3b10a30bcb14e234b11
김병곤 <fharenheit@gmail.com>
2018-09-08 10:35:46 +0900

    Discovery Client 추가

--- spring-cloud-eureka-client/src/main/java/com/example/springcloudeurekaclient/SpringCloudEurekaClientApplication.java
+++ spring-cloud-eureka-client/src/main/java/com/example/springcloudeurekaclient/SpringCloudEurekaClientApplication.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class SpringCloudEurekaClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
+    }
+
 }
